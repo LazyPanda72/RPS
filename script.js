82007e8ebeebdfb852498b1046cffa62939ba35a
LazyPanda72 <222779042+LazyPanda72@users.noreply.github.com>
2025-09-09 12:13:49 +0530

    done step 2

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    //math.random returns number equal or greater than 0 or less than 1
+    //multiply by 3 to get numbers upto 2
+    //math.floor rounds them up to 0, 1 or 2
+    const randomIndex = Math.floor(Math.random() * 3);
+    return(choices[randomIndex]);
+}
+
+console.log(getComputerChoice());
